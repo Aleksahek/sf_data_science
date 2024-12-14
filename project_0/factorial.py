ba0418592c0ebbfabbc70b605d5ccfd195c4feee
Aleksahek <sashkoan@yandex.ru>
2024-12-15 00:44:07 +0300

    replace files from atach in the task

--- project_0/factorial.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python3
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
-
-number = int(input("Введите число: "))
-print("Факториал числа", number, "равен", factorial(number))
